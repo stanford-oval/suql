db3e7bf31d060b254b73361c30aaac85acde9969
GeorgeL <46696710+george1459@users.noreply.github.com>
2023-05-12 16:56:22 -0700

    add server functionality to reviews

--- filter_reviews.py
+++ filter_reviews.py
@@ -2,6 +2,7 @@ from transformers import AutoModel, AutoTokenizer
 import pymongo
 from typing import List, Dict
 import os
+from flask import request, Flask
 
 model = AutoModel.from_pretrained("OpenMatch/cocodr-base-msmarco")
 tokenizer = AutoTokenizer.from_pretrained("OpenMatch/cocodr-base-msmarco")
@@ -11,6 +12,11 @@ client = pymongo.MongoClient(mongo)
 db = client['yelpbot']
 collection = db['yelp_data']
 
+# Set the server address
+host = "127.0.0.1"
+port = 8500
+review_server_address = 'http://{}:{}/query'.format(host, port)
+app = Flask(__name__)
 
 def filter_reviews(restaurants: List[str], keyword: str) -> List[str]:
     """
@@ -45,10 +51,32 @@ def filter_reviews(restaurants: List[str], keyword: str) -> List[str]:
     num_to_get = min(len(similarities), 5)
 
     top_restaurants = similarities[:num_to_get]
-    print(top_restaurants)
     rest_recommendations = set()
 
     for r in top_restaurants:
-        rest_recommendations.add(r[2])
+        # since reviews can only be of type String in Genie, the returned result will separate reviews by `\t`
+        # thus, we make sure here that the review text does not contain `\t`
+        rest_recommendations.add((r[0], r[2].replace('\t', '')))
 
     return list(rest_recommendations)
+
+@app.route('/query', methods=['POST'])
+def query():
+    data = request.get_json()
+    
+    # input params in this `data`    
+    # data["keyword"] : keyword to query
+    # data["restaurant_ids"] : list of restaurant ids to query reviews
+    # data["num_max_restaurants"] : max number of restaurants to query (optional)
+
+    if "keyword" not in data or "restaurant_ids" not in data:
+        return None
+    
+    num_max_restaurants = data["num_max_restaurants"] if "num_max_restaurants" in data else 5
+
+    res = filter_reviews(data["restaurant_ids"][:num_max_restaurants], data["keyword"])
+    print(res)
+    return res
+
+if __name__ == "__main__":
+    app.run(host=host, port=port)
