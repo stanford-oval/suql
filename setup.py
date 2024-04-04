1acdae841dd0d844c779b0587b0ae451f6787521
Shicheng Liu <liushicheng_george@outlook.com>
2024-04-04 04:31:30 +0000

    add `__init__.py`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 # Package metadata
 name = "suql"
-version = "1.0.0a0"
+version = "1.0.0a2"
 description = "Structured and Unstructured Query Language (SUQL) Python API"
 author = "Shicheng Liu"
 author_email = "shicheng@cs.stanford.edu"
