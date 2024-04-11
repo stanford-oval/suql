88e058803b6031a79e63ca6acba006617020ceec
Shicheng Liu <liushicheng_george@outlook.com>
2024-04-11 06:00:59 +0000

    `1.1.1` release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 # Package metadata
 name = "suql"
-version = "1.1.1a0"
+version = "1.1.1"
 description = "Structured and Unstructured Query Language (SUQL) Python API"
 author = "Shicheng Liu"
 author_email = "shicheng@cs.stanford.edu"
